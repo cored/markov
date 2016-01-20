79400224fd18c267f70a7b8bf6832c7fd959dfc9
Rafael George <george.rafael@gmail.com>
2016-01-19 21:34:20 -0500

    Initial commit

---
+++ profiles.clj
@@ -0,0 +1,4 @@
+{:dev {:env {:app-consumer-key "VW0tyhYVVusxPwvcgyCZVIHz3"
+             :app-consumer-secret "jakb3azecBXMz5e13VyjSocpoZm2zT6OVCsAWaag6D84Xmap77"
+             :user-access-token "54991971-6FtSRSr0U4ltg0lJ1TI5zhDHDnXJnPisv8KB31f0n"
+             :user-access-secret "31fli95MITkK5kloi9ILZP7Q4IuhYl6NxKNdLfyXQdZJX"}}}
